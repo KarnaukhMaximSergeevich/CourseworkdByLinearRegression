ac46e3600fc8ef2fd6a1f606eb2f6fa6a9ee0670
Max <Maximaximus18@gmail.com>
2024-03-18 14:56:35 +0200

    Base construction

---
+++ predict_bitcoin_course.py
@@ -0,0 +1,19 @@
+import numpy
+import matplotlib
+import pandas
+from sklearn.linear_model import LinearRegression
+
+
+class PredictBitcoinCourse:
+    """It's class predict bitcoin course by help database and linear regression"""
+
+    def __init__(self):
+        self.execute_function()
+
+    def execute_function(self):
+        """It's function execute other function in this class"""
+
+        pass
+
+
+class_model = PredictBitcoinCourse()
